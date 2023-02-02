4f326c2453c58f9f59add3423120a59f194acbba
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-02-02 22:42:09 +0000

    A method of generating hashed passwords is appended.
    -The user can only utilize the program if one of the pre-defined
    passwords is provided.

--- Locker/src/main/java/com/raman/main/AccessController.java
+++ Locker/src/main/java/com/raman/main/AccessController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import raman.Converter.BinarySystemConverter;
 
 public class AccessController implements EventHandler<ActionEvent>
 {
@@ -33,6 +34,33 @@ public class AccessController implements EventHandler<ActionEvent>
 			System.out.println("Close");
 		if(button == passwordController.getConfirmButton())
 			System.out.println("Confirm");
+	}
+	
+	/**
+	 * <h1> Generating Timed Passwords </h1>
+	 * <p> 
+	 * 		On call to this method, a list of strong hard coded passwords will be hashed with SHA-512 algorithm.
+	 * 		This method is only called internally during the creation of passwords file process normally called
+	 * 		once only unless the file is removed and the program had to regenerate it.
+	 * </p>
+	 * @return String[] The list of hashed passwords.
+	 */
+	private String[] getPasswords()
+	{
+		String[] passwords = new String[]{"Password231!", "Test231!"};
+		
+		String plaintext = "";
+		
+		for(int i = 0; i < passwords.length; i++)
+		{
+			//Hashing the password at given index. 
+			byte[] cipher = hasher.get_hash(passwords[i]);
+			//Converting the hashed value into hex format.
+			plaintext = BinarySystemConverter.convert_bytes_array_to_hex(cipher);
+			//Replacing the plain text password with the hashed version.
+			passwords[i] = plaintext;
+		}		
+		return passwords;
 	}
 	
 	// ############################# Toast Handler #############################
