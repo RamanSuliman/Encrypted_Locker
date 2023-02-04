9d7a03850041590e98186185c3e7a79d6f81ab68
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-02-04 16:49:12 +0000

    Refactoring and solving stage centering issue.

--- Locker/src/main/java/com/raman/fxfunctions/WindowService.java
+++ Locker/src/main/java/com/raman/fxfunctions/WindowService.java
@@ -11,21 +11,19 @@ public class WindowService
 {
 	private static double xOffset = 0;
 	private static double yOffset = 0;
-
+	// Get the screen size
+	private static Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 	/***
 	 * Centre the program in the middle of the screen (MUST BE CALLED AFTER) .show() method.
 	 */
 	public static void centreWindow(Stage primaryStage)
 	{
-		// Get the screen size
-        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         // Center the stage horizontally
         primaryStage.setX((screenBounds.getWidth() - primaryStage.getWidth()) / 2);
 
         // Center the stage vertically
         primaryStage.setY((screenBounds.getHeight() - primaryStage.getHeight()) / 2);
 	}
-
 	
 	/***
 	 * Centring the toast stage in the middle of the screen.
@@ -33,8 +31,14 @@ public class WindowService
 	public static void centreStage(Stage parent, Stage child)
 	{
 		// Calculate the center position of the parent Stage
-        double centerXPosition = parent.getX() + parent.getWidth()/2d;
-        double centerYPosition = parent.getY() + parent.getHeight()/2d;
+		double parentX = (Double.isNaN(parent.getX())? 0 : parent.getX());
+		double parentY = (Double.isNaN(parent.getY())? 0 : parent.getY());
+		//If parent stage width and height are 0 then use the screen mreasurments to center the child.
+		double parentWidth = (Double.isNaN(parent.getWidth())? screenBounds.getWidth() : parent.getWidth());
+		double parentHeight = (Double.isNaN(parent.getHeight())? screenBounds.getHeight() : parent.getHeight());
+		
+        double centerXPosition = parentX + parentWidth / 2d;
+        double centerYPosition = parentY + parentHeight / 2d;
 
         // Relocate the pop-up Stage
         child.setOnShown(ev -> {
